53e395679d864a420ed077aa952467168f622562
Camila Sáez <cfsaezbravo@gmail.com>
2017-06-21 12:55:56 -0400

    Footer ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
-main.js
+function initMap() {
+	var map = new google.maps.Map(document.getElementById("map"), {
+		zoom: 5,
+		center: {lat: -9.1191427, lng: -77.0349046},
+		mapTypeControl: false,
+		zoomControl: false,
+		streetViewControl: false
+	});
+
+	function buscar(){
+		if(navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+		}
+	}
